650d14bf9c3b1dcc93e0c49bcebbffe02d2fea3b
Shihab177 <shihab7777h@gmail.com>
2025-11-27 21:59:30 +0600

    [Update]:signUp component ui design improve

--- src/pages/authentication/SignUp/SignUp.jsx
+++ src/pages/authentication/SignUp/SignUp.jsx
@@ -4,8 +4,7 @@ import Logo from "../../../shared/Logo/Logo";
 import { Link } from "react-router";
 const SignUp = () => {
   return (
-    <div className="min-h-screen flex">
-      {/* LEFT SECTION */}
+    <div className="min-h-screen flex poppins">
       <div className="w-[76%] bg-[#f5f7fa] px-12 py-8">
         <Link to="/">
           {" "}
@@ -16,16 +15,14 @@ const SignUp = () => {
         </div>
       </div>
 
-      {/* RIGHT SECTION */}
       <div className="w-[24%] flex flex-col justify-center items-center px-10">
         <h2 className="text-2xl font-bold text-[#46566B]">
-          Welcome To Lineone
+          Welcome To E-School
         </h2>
         <p className="text-sm text-gray-400 mb-8">Please sign up to continue</p>
 
-        {/* Social Login */}
         <div className="flex gap-4 mb-5 w-full">
-          <button className="border border-gray-300  px-6 h-10 w-[50%] rounded-lg flex items-center justify-center gap-2 hover:bg-[#E9EEF5]">
+          <button className="border border-gray-300  px-6 h-10 w-[50%] rounded-lg  cursor-pointer flex items-center justify-center gap-2 hover:bg-[#E9EEF5]">
             <img
               width="20"
               src="https://www.svgrepo.com/show/475656/google-color.svg"
@@ -33,7 +30,7 @@ const SignUp = () => {
             Google
           </button>
 
-          <button className="border border-gray-300 px-6 h-10 w-[50%] rounded-lg flex items-center justify-center gap-2 hover:bg-[#E9EEF5]">
+          <button className="border border-gray-300 px-6 h-10 w-[50%] rounded-lg  cursor-pointer flex items-center justify-center gap-2 hover:bg-[#E9EEF5]">
             <img
               width="20"
               src="https://www.svgrepo.com/show/349375/github.svg"
@@ -51,8 +48,7 @@ const SignUp = () => {
           <div className="flex-grow border-t border-gray-300"></div>
         </div>
 
-        {/* Form */}
-        <form className="w-full space-y-4">
+        <form className="w-full space-y-4 mt-4">
           <input
             type="text"
             className="w-full border border-gray-300 rounded-lg h-10 px-3 text-sm focus:outline-none focus:ring-2 focus:ring-[#A7A3EF]"
@@ -78,14 +74,14 @@ const SignUp = () => {
             <input type="checkbox" />I agree with privacy policy
           </label>
 
-          <button className="w-full h-11 rounded-lg bg-[#4f46e5] text-white">
+          <button className="w-full mt-5 h-11 rounded-lg bg-[#4f46e5] text-white cursor-pointer hover:scale-105 active:scale-95">
             Sign In
           </button>
         </form>
 
         <p className="text-sm text-gray-500 mt-4">
           Already have an account?{" "}
-          <span className="text-blue-500 cursor-pointer">Sign In</span>
+          <Link to="/signin"><span className="text-blue-500 cursor-pointer ">Sign In</span></Link>
         </p>
       </div>
     </div>
