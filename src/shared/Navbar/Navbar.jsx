40901d6f607f13d524bbe4b8ca7ccbfda75f332f
Shihab177 <shihab7777h@gmail.com>
2025-11-27 17:11:59 +0600

    [Update]:Navbar add top padding and hover underline for links

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import logo from "../../assets/images/logo (6).png";
 const Navbar = () => {
   return (
-    <header className="bg-[#F0FFF0]">
-      <nav className="max-w-7xl mx-auto flex items-center justify-between">
+    <header className="bg-[#F0FFF0] pt-[30px]">
+      <nav className="max-w-7xl mx-auto flex items-center justify-between ">
         <div className=" flex items-center gap-x-2">
           <img className="w-18 h-14" src={logo} alt="logo" />
          <h1 className="text-[#56548C] text-2xl font-bold">E- School</h1>
         </div>
         <div className="flex items-center gap-x-10">
           <ul className="flex items-center gap-x-8 text-[#000000] text-base font-normal ">
-            <li className="cursor-pointer">Books</li>
-            <li className="cursor-pointer">Courses</li>
-            <li className="cursor-pointer">Others</li>
-            <li className="cursor-pointer">Blog</li>
+            <li className="cursor-pointer hover:underline">Books</li>
+            <li className="cursor-pointer hover:underline">Courses</li>
+            <li className="cursor-pointer hover:underline">Others</li>
+            <li className="cursor-pointer hover:underline">Blog</li>
           </ul>
           <button className="bg-[#21B573] hover:bg-[#1A955C] rounded-[33px] shadow-sm text-[#FFFFFF] text-base font-normal py-3 px-7 cursor-pointer transition-colors duration-200">Sing in</button>
         </div>
