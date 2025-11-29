9f1a6fd9bc3544ba91bd28b856522fc43294cc6c
Shihab177 <shihab7777h@gmail.com>
2025-11-29 19:36:37 +0600

    [add]:QueryClientProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import { RouterProvider } from 'react-router'
-import { router } from './Router/Router.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode >
-  <div className='font-rubik bg-[#FFFFFF]'>
-     <RouterProvider router={router}/>
-  </div>
-  </StrictMode>,
-)
+import { RouterProvider } from "react-router";
+import { router } from "./Router/Router.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <div className="font-rubik bg-[#FFFFFF]">
+        <RouterProvider router={router} />
+      </div>
+    </QueryClientProvider>
+  </StrictMode>
+);
