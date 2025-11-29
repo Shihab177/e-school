3114661bd471590bec3e49beac20332238482eda
Shihab177 <shihab7777h@gmail.com>
2025-11-29 19:35:26 +0600

    [update]: add dashboard routes with AdminRoute, error and forbidden pages

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,7 +4,12 @@ import Home from "../pages/home/Home";
 import AuthLayout from "../layout/AuthLayout";
 import Signup from "../pages/authentication/SignUp/SignUp";
 import Signin from "../pages/authentication/SignIn/SignIn";
-
+import DashboardLayout from "../layout/DashboardLayout";
+import Overview from "../components/overview/Overview";
+import AllUsers from "../components/AllUsers/AllUsers";
+import ErrorPage from "../pages/errorPage/ErrorPage";
+import AdminRoute from "../routes/adminRoute";
+import Forbidden from "../pages/forbidden/Forbidden";
 
 export const router = createBrowserRouter([
   {
@@ -26,9 +31,32 @@ export const router = createBrowserRouter([
         Component: Signup,
       },
       {
-        path:"signin",
-        Component:Signin
-      }
+        path: "signin",
+        Component: Signin,
+      },
     ],
+  },
+  {
+    path: "/dashboard",
+    element:<AdminRoute><DashboardLayout/></AdminRoute>,
+   
+    children: [
+      {
+        index: true,
+        Component: Overview,
+      },
+      {
+        path: "allusers",
+        Component: AllUsers,
+      },
+    ],
+  },
+  {
+    path: "/forbidden",
+    Component:Forbidden
+  },
+  {
+    path: "*",
+    Component: ErrorPage,
   },
 ]);
