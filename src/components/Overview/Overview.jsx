e462c7a9e2997d30791dc7851b980a7c7569d0aa
Shihab177 <shihab7777h@gmail.com>
2025-11-29 19:38:00 +0600

    [Add]admin dashboard  overview page

---
+++ src/components/Overview/Overview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Overview = () => {
+    return (
+        <div>
+            <h1 className='flex items-center justify-center text-6xl'>Overview</h1>
+        </div>
+    );
+};
+
+export default Overview;
